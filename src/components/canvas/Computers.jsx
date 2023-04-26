fc1ee40b26fba4ce4c7af14cc38f4cb9ac1caeed
Justin Johnson <threejay20@yahoo.ca>
2023-04-26 11:32:12 -0400

    stcuk at computer error

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,35 +1,34 @@
-import {Suspense, useEffect, useState} from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-import CanvasLoader from '../Loader';
+import CanvasLoader from "../Loader";
 
-const Computers = () => {
-  const computer = useGLTF('./desktop_pc/scene.gltf')
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("src/public/desktop_pc/scene.gltf");
+
   return (
     <mesh>
-
-      <hemisphereLight intensity={0.15}
-      groundColor="black" />
-
-      <pintLight intensity={1} />
-      <primitive object={computer.scene}/>
-
-
+      <hemisphereLight intensity={0.15} 
+      groundColor='black' 
+      />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
-  )
-}
-const ComputersCanvas = () => {
-  return (
-    <Canvas
-    frameloop="demand"
-    shadows
-    camera
-    >
-
-
-    </Canvas>
-  )
-}
+  );
+};
 
 export default Computers
