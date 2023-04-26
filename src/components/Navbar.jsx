09933032a500a3c2dadcb5e0edaf9b8b0aab3891
Justin Johnson <threejay20@yahoo.ca>
2023-04-26 10:44:51 -0400

    slight change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
 
         </Link>
 
-        <ul className="list none hidden sm:flex flex-row gap-10">
+        <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
             <li
             key={link.id}
@@ -56,11 +56,11 @@ const Navbar = () => {
         onClick={() => setToggle(!toggle)}
         />
         
-        <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute 
+        <div className={`${!toggle ? "hidden" : "flex"} p-6 black-gradient absolute 
         top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
 
 
-          <ul className="list none flex justify-end items-start flex-col gap-4">
+          <ul className="list none flex justify-end items-start flex-1 flex-col gap-4">
           {navLinks.map((link) => (
             <li
             key={link.id}
