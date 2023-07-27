7aa4ec0a3657c4111e158fc7c1786b8b6c377d34
threejay20 <118399217+threejay20@users.noreply.github.com>
2023-07-27 16:30:32 -0400

    Add project_3D_developer_portfolio

---
+++ project_3D_developer_portfolio/src/assets/index.js
@@ -0,0 +1,75 @@
+import logo from "./newlogo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import Bell_logo from "./company/Bell_logo.png";
+import Lyriks from "./tech/Lyriks.png";
+import Title_Exercise from "./tech/Title_Exercise.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import Finanseer from "./tech/Finanseer.png";
+import AdminApp from "./tech/AdminApp.png";
+import RealEstateApp from "./tech/RealEstateApp.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  Bell_logo,
+  Lyriks,
+  Title_Exercise,
+  Finanseer,
+  AdminApp,
+  RealEstateApp,
+};
---
+++ project_3D_developer_portfolio/src/constants/index.js
@@ -0,0 +1,318 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  threejs,
+  Bell_logo,
+  Lyriks,
+  Title_Exercise,
+  Finanseer,
+  AdminApp,
+  RealEstateApp,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  // {
+  //   title: "React.js Developer",
+  //   company_name: "Starbucks",
+  //   icon: starbucks,
+  //   iconBg: "#383E56",
+  //   date: "March 2020 - April 2021",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  // {
+  //   title: "React Native Developer",
+  //   company_name: "Tesla",
+  //   icon: tesla,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2021 - Feb 2022",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  // {
+  //   title: "Web Developer",
+  //   company_name: "Shopify",
+  //   icon: shopify,
+  //   iconBg: "#383E56",
+  //   date: "Jan 2022 - Jan 2023",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  {
+    title: "Full Stack Developer",
+    company_name: "Bell Canada",
+    icon: Bell_logo,
+    iconBg: "#E6DEDD",
+    date: "March 2023 - Present",
+    points: [
+      "Directed full-stack web application development using HTML, JavaScript, CSS, and Java, ensuring high-quality code delivery.",
+      "Collaborated with cross-functional teams to design and implement scalable software solutions,fostering innovation.",
+      "Implemented comprehensive code review processes to maintain code quality standards.",
+      "Actively participated in Agile development processes, ensuring streamlined project execution.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Justin proved me wrong.",
+    name: "Sara Lee",
+    // designation: "CFO",
+    // company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Justin does.",
+    name: "Chris Brown",
+    // designation: "COO",
+    // company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Justin optimized our website, our traffic increased by 50%. We can't thank him enough!",
+    name: "Lisa Wang",
+    // designation: "CTO",
+    // company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Lyriks",
+    description:
+      "Web-based platform that allows users to search, play, and sing along with their favorite artists.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "rapidApi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: Lyriks,
+    source_code_link: "https://github.com/threejay20/Lyriks.git",
+  },
+
+  {
+    name: "Exercise App",
+    description:
+      "Web-based platform that allows users to search, learn, and watch specific exercises to ensure maximum fitness success.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "rapidApi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: Title_Exercise,
+    source_code_link: "https://github.com/threejay20/gym_exercises.git",
+  },
+  {
+    name: "Finanseer",
+    description:
+      "Web application that keeps track of companies profits as well as predicts future revenue through simple Machine Learning.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Typescript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Mongo-Db",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Node",
+        color: "blue-text-gradient",
+      }
+    ],
+    image: Finanseer,
+    source_code_link: "https://github.com/threejay20/finance-app.git",
+  },
+  {
+    name: "AdminApp",
+    description:
+      "Web application that helps manage a company's results",
+    tags: [
+      {
+        name: "React",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Data Grid",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Material UI",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Node",
+        color: "blue-text-gradient",
+      }
+    ],
+    image: AdminApp,
+    source_code_link: "https://github.com/threejay20/react-admin",
+  },
+  {
+    name: "Real Estate",
+    description:
+      "A comprehensive property listing site for any Real estate broker.",
+    tags: [
+      {
+        name: "nextjs 13",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: RealEstateApp,
+    source_code_link: "https://github.com/threejay20/RealEstate.git",
+  },
+];
+
+
+
+
+export { services, technologies, experiences, testimonials, projects };
